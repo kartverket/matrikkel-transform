01d381e56ce379a76487504ece924363995a30dd
Fredrik de Vibe <fredrik.de.vibe@kartverket.no>
2019-11-30 00:41:45 +0100

    Gjenbruk delt bibliotek dersom det er identisk og ikke i bruk, ellers opprett nytt
    Det er ikke mulig å rydde opp installert bibliotek vha. deleteOnExit(), siden
    JNI-biblioteker ikke unloades før classloaderen garbage-collectes, og deleteOnExit()
    (kan) kalles før bibliotekene unloades. På Windows er disse filene låst og vil derfor
    ikke kunne slettes når deleteOnExit() kalles, og vi får ikke ryddet opp ordentlig.
    
    Ny løsning, i samarbeid med Øystein Winge, er å først se om biblioteket er installert
    under java.io.tmpdir. Hvis det er det, sjekk at det er identisk med bundlet versjon,
    via SHA-256. Hvis alt så ser greit ut, forsøk å laste biblioteket. Hvis en annen
    classloader allerede har lastet dette vil lastingen feile, og vi går videre og
    oppretter til slutt en ny installasjon og laster denne.

--- src/main/java/no/statkart/matrikkel/persistens/coordtransform/SkTrans.java
+++ src/main/java/no/statkart/matrikkel/persistens/coordtransform/SkTrans.java
@@ -4,48 +4,53 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 /**
- * Klasse som kommuniserer med transformasjons-dll via et jni-kall.
+ * Klasse som kommuniserer med transformasjonsbiblioteket via et jni-kall.
  *
  * @author Roar Ingebrigtsen
  */
 public class SkTrans {
-
     private static final Logger logger = LoggerFactory.getLogger(SkTrans.class);
-    private static final String INIT_PATH = "transformation_init";
-
+    private static final String INSTALL_DIRECTORY_INFIX = "statkart/matrikkelen";
     private static boolean isOnLinux = System.getProperty("os.name").equals("Linux");
-    private String transformationPath;
-    private static final Filename linuxLibrary = new Filename("libsositrans", "so");
-    private static final Filename windowsLibrary = new Filename("SosiTransformasjon", "dll");
-    private static Filename[] libraries = new Filename[]{
-            new Filename("libifcoremd", "dll"),
-            new Filename("libmmd", "dll"),
-            new Filename("libSosiTransformasjon", "a"),
+    private final String DIGEST_ALGORITHM = "SHA-256";
+    private final String TRANSFORMATION_INIT_DIR_NAME = "transformation_init";
+    private final String LIB_DIR_NAME = "lib";
+    private static Filename linuxLibrary = new Filename("libsositrans.so");
+    private static Filename windowsLibrary = new Filename("SosiTransformasjon.dll");
+    private static Filename [] libraries = new Filename[] {
             linuxLibrary,
-            new Filename("skt2lan1_64", "dll"),
             windowsLibrary,
-            new Filename("svml_dispmd", "dll")
+            new Filename("libifcoremd.dll"),
+            new Filename("libmmd.dll"),
+            new Filename("libSosiTransformasjon.a"),
+            new Filename("skt2lan1_64.dll"),
+            new Filename("svml_dispmd.dll")
     };
-    private static Filename[] initFiles = new Filename[]{
-            new Filename("arcgp-2006-sk", "bin"),
-            new Filename("HREF2016B_NN2000_EUREF89", "bin"),
-            new Filename("lan1_fellesp_20081014", "bin"),
-            new Filename("Milne_east", "bin"),
-            new Filename("NNTrans2016B", "bin"),
-            new Filename("Uplift_Svalbard_pdim_pgs_tot", "dat"),
-            new Filename("href2008a", "bin"),
-            new Filename("IGS05N_EUREF89_7PAR_2013", "txt"),
-            new Filename("lan1_fellesp", "bin"),
-            new Filename("Milne_north", "bin"),
-            new Filename("RH2000LU_absup", "bin")
+    private static Filename[] initFiles = new Filename[] {
+            new Filename("arcgp-2006-sk.bin"),
+            new Filename("HREF2016B_NN2000_EUREF89.bin"),
+            new Filename("lan1_fellesp_20081014.bin"),
+            new Filename("Milne_east.bin"),
+            new Filename("NNTrans2016B.bin"),
+            new Filename("Uplift_Svalbard_pdim_pgs_tot.dat"),
+            new Filename("href2008a.bin"),
+            new Filename("IGS05N_EUREF89_7PAR_2013.txt"),
+            new Filename("lan1_fellesp.bin"),
+            new Filename("Milne_north.bin"),
+            new Filename("RH2000LU_absup.bin")
     };
 
     private native int xSosiTrans(int fraKoordSys, double fraX, double fraY, double fraH, int tilKoordSys, double[] returTall);
@@ -53,7 +58,8 @@ public class SkTrans {
     private native String getLastError();
 
     SkTrans() {
-        loadDll();
+        setupAndLoadLibrary();
+        initializeLibrary();
     }
 
     private void copyFile(String dir, Filename filename, Path destination) {
@@ -67,7 +73,6 @@ public class SkTrans {
             Path target = Paths.get(destination.toString(), filename.name());
             Files.copy(istream, target, StandardCopyOption.REPLACE_EXISTING);
             filename.setInstalledPath(target.toFile());
-            registerFileForCleanup(new File(filename.getInstalledPath().toString()));
         } catch (IOException e) {
             logger.error("Failed to copy file {} from {} to {}", filename.name(), dir, destination.toString(), e);
             logger.error("istream: {}", istream);
@@ -78,73 +83,22 @@ public class SkTrans {
     }
 
     private void copyFiles(Path tmpDir) {
-        Path initDir = Paths.get(tmpDir.toString(), "transformation_init");
+        Path initDir = Paths.get(tmpDir.toString(), TRANSFORMATION_INIT_DIR_NAME);
         try {
-            initDir = Files.createDirectory(initDir);
-            registerFileForCleanup(initDir.toFile());
+            Files.createDirectory(initDir);
         } catch (IOException e) {
             logger.error("Failed to create temporary directory structure", e);
             throw new RuntimeException(e);
         }
         for (Filename lib: libraries) {
-            copyFile("lib", lib, tmpDir);
+            copyFile(LIB_DIR_NAME, lib, tmpDir);
         }
         for (Filename initFile: initFiles) {
-            copyFile("transformation_init", initFile, initDir);
+            copyFile(TRANSFORMATION_INIT_DIR_NAME, initFile, initDir);
         }
     }
 
-    private void registerFileForCleanup(File file) {
-        logger.debug("Registering {} for cleanup", file.toString());
-        file.deleteOnExit();
-    }
-
-    private void setupNativeFiles() {
-        Path tmpdir = null;
-        try {
-            tmpdir = Files.createTempDirectory("sktrans-lib");
-            registerFileForCleanup(tmpdir.toFile());
-            logger.info("Created temporary directory {}", tmpdir.getFileName());
-        } catch (IOException e) {
-            logger.error("Failed to create temporary directory", e);
-            throw new RuntimeException(e);
-        }
-        try {
-            copyFiles(tmpdir);
-        } catch (RuntimeException e) {
-            logger.warn("Attempting to remove temporary directory {} since exception '{}' was thrown",
-                    tmpdir.getFileName(), e.getMessage());
-            //noinspection ResultOfMethodCallIgnored
-            tmpdir.toFile().delete();
-            throw e;
-        }
-    }
-
-    private static class Filename {
-        private final String prefix;
-        private final String suffix;
-        private File installedPath;
-
-        Filename(String prefix, String suffix) {
-            this.prefix = prefix;
-            this.suffix = suffix;
-        }
-
-        String name() {
-            return prefix + "." + suffix;
-        }
-
-        void setInstalledPath(File path) {
-            installedPath = path;
-        }
-
-        File getInstalledPath() {
-            return installedPath;
-        }
-    }
-
-    private void loadDll() {
-        setupNativeFiles();
+    private void loadLibraryOrCroak() {
         String libPath = getTransformationLibrary().getInstalledPath().toString();
         try {
             System.load(libPath);
@@ -156,6 +110,134 @@ public class SkTrans {
             String msg = "Lasting av " + libPath + " på server feilet:  " + e.getMessage();
             throw new RuntimeException(msg, e);
         }
+    }
+
+    private boolean tryLoadLibrary() {
+        String libPath = getTransformationLibrary().getInstalledPath().toString();
+        try {
+            System.load(libPath);
+            return true;
+        } catch (UnsatisfiedLinkError e) {
+            return false;
+        }
+    }
+
+    private void setupAndLoadLibrary() {
+        String libraryPathName = System.getProperty("no.statkart.matrikkel.transform.library.path");
+        if (libraryPathName == null) {
+            libraryPathName = System.getProperty("java.io.tmpdir");
+            if (libraryPathName == null) {
+                throw new RuntimeException("Neither java.io.tmpdir nor no.statkart.matrikkel.transform.library.path is set, cannot continue");
+            }
+            int counter = 1;
+            while (true) {
+                Path libraryPath = Paths.get(libraryPathName, INSTALL_DIRECTORY_INFIX, String.valueOf(counter));
+                if (Files.exists(libraryPath)) {
+                    if (testPath(libraryPath) && tryLoadLibrary()) {
+                        return;
+                    }
+                    counter++;
+                } else {
+                    buildPath(libraryPath);
+                    loadLibraryOrCroak();
+                    return;
+                }
+            }
+        }
+        throw new RuntimeException("Reached unreachable state");
+    }
+
+    private boolean testPath(Path path) {
+        for (Filename lib: libraries) {
+            if (testFile(path.toString(), lib, LIB_DIR_NAME)) {
+                return false;
+            }
+        }
+        for (Filename initFile: initFiles) {
+            if (testFile(Paths.get(path.toString(), TRANSFORMATION_INIT_DIR_NAME).toString(), initFile, TRANSFORMATION_INIT_DIR_NAME)) {
+                return false;
+            }
+        }
+        logger.info("Library path {} matches package, reusing existing", path);
+        return true;
+    }
+
+    private boolean testFile(String filesystemDir, Filename filename, String jarDir) {
+        File fileSystemPath = Paths.get(filesystemDir, filename.name()).toFile();
+        if (!fileSystemPath.exists()) {
+            logger.debug("Couldn't find filesystem path for {}", fileSystemPath);
+            return true;
+        }
+        filename.setInstalledPath(fileSystemPath);
+        String jarPath = "/" + jarDir + "/" + filename.name();
+        InputStream jarStream = SkTrans.class.getResourceAsStream(jarPath);
+        if (jarStream == null) {
+            throw new RuntimeException(String.format("Failed to get input stream from %s in %s",
+                    jarPath, this.getClass().getResource("SkTrans.class")));
+        }
+        try {
+            MessageDigest jarDigester = MessageDigest.getInstance(DIGEST_ALGORITHM);
+            DigestInputStream jarDis = new DigestInputStream(jarStream, jarDigester);
+            MessageDigest fileDigester = MessageDigest.getInstance(DIGEST_ALGORITHM);
+            InputStream fileStream = new FileInputStream(fileSystemPath);
+            DigestInputStream fileDis = new DigestInputStream(fileStream, fileDigester);
+            byte[] fileDigest = fileDis.getMessageDigest().digest();
+            byte[] jarDigest = jarDis.getMessageDigest().digest();
+            if (Arrays.equals(fileDigest, jarDigest)) {
+                return false;
+            }
+            logger.debug("Filesystem file {} ({}) does not match jar file {} ({})", fileSystemPath, fileDigest, jarPath, jarDigest);
+            return true;
+        } catch (IOException e) {
+            logger.error("Failed to compare files {} and {}: {}", fileSystemPath.toString(), jarPath, jarDir, e);
+            throw new RuntimeException(e);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void buildPath(Path path) {
+        try {
+            Files.createDirectories(path);
+            logger.info("Created SkTrans directory {}", path.getFileName());
+        } catch (IOException e) {
+            logger.error("Failed to create directory {}", path.toString(), e);
+            throw new RuntimeException(e);
+        }
+        try {
+            copyFiles(path);
+        } catch (RuntimeException e) {
+            logger.warn("Attempting to remove directory {} since exception '{}' was thrown",
+                    path.getFileName(), e.getMessage());
+            //noinspection ResultOfMethodCallIgnored
+            path.toFile().delete();
+            throw e;
+        }
+    }
+
+    private static class Filename {
+        private final String name;
+        private File installedPath;
+
+        Filename(String name) {
+            this.name = name;
+        }
+
+        String name() {
+            return name;
+        }
+
+        void setInstalledPath(File path) {
+            installedPath = path;
+        }
+
+        File getInstalledPath() {
+            return installedPath;
+        }
+    }
+
+    private void initializeLibrary() {
+        String libPath = getTransformationLibrary().getInstalledPath().toString();
         logger.debug("Lasting ok, initialiserer " + libPath);
         System.err.println("Lasting ok, initialiserer " + libPath);
 
