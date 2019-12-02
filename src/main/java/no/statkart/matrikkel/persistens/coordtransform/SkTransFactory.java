933f22ebc440d28c33775a798f6a6a3b08831218
Fredrik de Vibe <fredrik.de.vibe@kartverket.no>
2019-12-02 23:12:06 +0100

    Gjør initialisering fra SkTransFactory thread safe

--- src/main/java/no/statkart/matrikkel/persistens/coordtransform/SkTransFactory.java
+++ src/main/java/no/statkart/matrikkel/persistens/coordtransform/SkTransFactory.java
@@ -3,9 +3,11 @@ package no.statkart.matrikkel.persistens.coordtransform;
 public class SkTransFactory {
     private static SkTrans instance;
     public static SkTrans getInstance() {
-        if (instance == null) {
-            instance = new SkTrans();
+        synchronized (SkTransFactory.class) {
+            if (instance == null) {
+                instance = new SkTrans();
+            }
+            return instance;
         }
-        return instance;
     }
 }
